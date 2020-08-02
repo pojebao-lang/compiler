45e5a052f140d7ebe2d14af5d7d8c45f0e047f38
liskqu <56258709+lisqu16@users.noreply.github.com>
2020-08-02 11:54:55 +0200

    reading the file
    reading the file given as an argument to the console

---
+++ pojebao-compiler/pojebao-compiler/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace pojebao_compiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args[0].EndsWith(".pjb"))
+            {
+                string code = File.ReadAllText(args[0]);
+                Array kod = code.Split("\n");
+                foreach (string line in kod)
+                {
+                    if (1 == 2)
+                    {
+                        // command code
+                    }
+                    else
+                    {
+                        Console.Write("Error: Command " + line + " not recognized!");
+                    }
+                }
+            } else
+            {
+                Console.Write("Error: The file does not have the .pjb extension");
+            }
+        
+        }
+    }
+}
